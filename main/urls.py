12a6ae61a4ff027cdc05af834ec4a80afbb6db60
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-11-25 23:56:30 -0600

    add urls.py to main app

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path 
-from .import views
+from django.urls import path
+from . import views
 
 app_name = "main"
 
-urlpatterns = []
+urlpatterns = [
+    path('route', view.route, name="route"),
+    path('map', view.map, name="map"),
+]
